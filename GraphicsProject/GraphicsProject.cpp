09368aef89805431607ad6c701a09e822f4fcef4
koju <koju@LAPTOP-OIPV329O>
2022-08-21 20:45:47 +0545

    "animated" layer rotation

--- GraphicsProject/GraphicsProject.cpp
+++ GraphicsProject/GraphicsProject.cpp
@@ -15,7 +15,6 @@ int main()
 
     std::vector<mesh >Cube;
     Cube = LoadObject("simple2.mtl", "simple2.obj");
-    std::cout << Cube.size() << std::endl;
 
     Camera cam;
     cam.update({ 7, 7, 7, 1 }, {0, 0, 0, 1}); //for cube
@@ -35,7 +34,12 @@ int main()
     circle.setOrigin(sf::Vector2f(2.5, 2.5));
     circle.setPosition(sf::Vector2f(lightOrigin[0], lightOrigin[1]));
     std::vector<std::vector<float> > zbuffer(SCREEN_HEIGHT, std::vector<float>(SCREEN_WIDTH, INF));
-    std::vector<std::vector<Vec4> > nbuffer(SCREEN_HEIGHT, std::vector<Vec4>(SCREEN_WIDTH, Zero4));;
+    std::vector<std::vector<Vec4> > nbuffer(SCREEN_HEIGHT, std::vector<Vec4>(SCREEN_WIDTH, Zero4));
+    Vec4 normal;
+    std::vector<bool> transform_cubes(Cube.size(), false);
+    bool rotating = false;
+    bool leftRotate = true;
+    float angle = 0;
 
     while (window.isOpen())
     {
@@ -87,41 +91,36 @@ int main()
                     oldposition = { -1, -1, 1, 1 };
                 }
             }
-            else if (event.type == sf::Event::MouseButtonPressed && sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)) {
-                std::cout << event.key.control << std::endl;
+            else if (event.type == sf::Event::MouseButtonPressed && sf::Keyboard::isKeyPressed(sf::Keyboard::LControl) && !rotating) {
                 sf::Vector2i mouseposition = sf::Mouse::getPosition(window);
                 int i = mouseposition.x;
                 int j = mouseposition.y;
                 if (i < 0 || i >= SCREEN_WIDTH || j < 0 || j >= SCREEN_HEIGHT)continue;
-                Vec4 normal = nbuffer[j][i];
-                Mat4 T;
+                normal = nbuffer[j][i];
                 if (event.mouseButton.button == sf::Mouse::Left) {
-                    T = getRotationMatrix(cubecentre, cubecentre + normal, 90);
+                    leftRotate = true;
                 }
                 else {
-                    T = getRotationMatrix(cubecentre, cubecentre + normal, -90);
+                    leftRotate = false;
                 }
                 if (zbuffer[j][i] > 1 && zbuffer[j][i] < INF) {
-                    for (mesh& m : Cube) {
+                    for (int k = 0; k<Cube.size(); k++) {
                         bool flag = false;
-                        for (triangle t : m.triangles) {
+                        for (triangle t : Cube[k].triangles) {
                             Vec4 v1 = t.p1 - t.p0;
                             Vec4 v2 = t.p2 - t.p0;
                             Vec4 cross_product = v1 * v2;
                             if (magnitude(normal - normalize(cross_product)) < 0.0005 && t.mtl.name != "Material.001") {
                                 flag = true;
-                                material mat = t.mtl;
-                                std::cout << mat.name << std::endl;
+                                rotating = true;
                                 break;
                             }
                         }
-                        if (flag) {
-                            m.transform(T);
-                        }
+                        transform_cubes[k] = flag;
                     }
                 }
             }
-            else if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            else if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && !rotating) {
                 sf::Vector2i mouseposition = sf::Mouse::getPosition(window);
                 Vec4 newposition{ mouseposition.x, mouseposition.y, 1, 1};
 
@@ -149,6 +148,28 @@ int main()
         Mat4 T = getRotationMatrix(cubecentre, cubecentre+Vec4{0, 1, 0, 0}, 5 * theta);
         for(mesh &m:Cube)
             m.transform(T);
+
+
+        if (rotating) {
+            float rotate_angle = 90;
+            if (!leftRotate)rotate_angle = -90;
+            float rotate_time = 0.5;
+            float speed = rotate_angle/rotate_time;
+            Mat4 T;
+            angle += speed * t;
+            if (abs(angle) > 90) {
+                angle -= speed * t;
+                T = getRotationMatrix(cubecentre, cubecentre + normal, rotate_angle - angle);
+                angle = 0;
+                rotating = false;
+            }
+            else T = getRotationMatrix(cubecentre, cubecentre + normal, speed * t);
+            for (int i = 0; i < Cube.size(); i++) {
+                if (transform_cubes[i]) {
+                    Cube[i].transform(T);
+                }
+            }
+        }
 
 
 
